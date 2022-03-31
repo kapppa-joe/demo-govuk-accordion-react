bf99bfac4c564fdbf18e54a945e8e256f1b736e2
Joe Fong <joe.fong@madetech.com>
2022-03-31 20:04:00 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import './main.sass'
+import Accordion from './accordion';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="govuk-heading-l">Gov UK accordion in a react app</h1>
+      <section className='govuk-width-container'>
+        <Accordion />
+      </section>
     </div>
   );
 }
